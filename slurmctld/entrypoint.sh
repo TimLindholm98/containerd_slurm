52a4ebde4fb57d65beba9f27a7add11ca6579339
TimLindholm98 <tim@enginit.se>
2024-04-29 13:13:40 +0200

    Adding comments

--- slurmctld/entrypoint.sh
+++ slurmctld/entrypoint.sh
@@ -1,17 +1,19 @@
 #!/bin/bash
 set -x
-init(){
-   chmod 700 /var/spool/slurmctld
+
+correct_permissons(){
+    chmod 700 /var/spool/slurmctld
+}
+
+init_munge(){
    chown munge:munge /run/munge
    sudo -u munge /usr/sbin/munged -S /run/munge/munge.socket.2 -v &
 }
-export -f init
 
-init
 
-#if [ -n "$1" ] || [ "$1" != "init"  ]; then
-#    /bin/bash -c "$1"
-#else
-    /opt/slurm/sbin/slurmctld -Dv
-#fi
+init_sssd(){
+}
 
+export -f init_munge && init_munge
+
+/opt/slurm/sbin/slurmctld -Dv
